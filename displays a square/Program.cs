b81853086ae632b28caecf4ead612614567b6d97
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-03-21 11:06:35 +0300

    Добавьте файлы проекта.

---
+++ displays a square/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace displays_a_square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 5; // длина стороны квадрата
+            char symbol = '*'; // символ, используемый для отображения
+
+            DrawSquare(length, symbol);
+
+            Console.ReadLine();
+        }
+
+        static void DrawSquare(int length, char symbol)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
